03c08422bdddf18c1f8f0369afc15649594b42d4
Michael Kuts <mkuts12@gmail.com>
2016-02-14 18:55:11 +0200

    draft

--- src/album.js
+++ src/album.js
@@ -2,5 +2,23 @@ import React from 'react';
 import data from 'dataStore';
 
 export default class Album extends React.Component {
-    render
+    render(){
+        return (
+            <div>
+                <div>
+                    well this is a picture
+                </div>
+                <ul>
+                    {
+                        data.songs(this.props.name).map(song => {
+                            <li>
+                                song: {song}
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
+
+        )
+    }
 }
