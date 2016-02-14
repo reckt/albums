feadacb58124274b4855b6d03a6056184eb118fd
Michael Kuts <mkuts12@gmail.com>
2016-02-14 22:07:47 +0200

    fixed to correct param

--- src/album.js
+++ src/album.js
@@ -13,7 +13,7 @@ export default class Album extends React.Component {
                 <ul>
                     {
                         songs({
-                            albumName: this.props.params.name,
+                            albumName: this.props.params.album,
                         }).map(song => {
                             return (
                                 <li>
