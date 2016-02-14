0dc059c393544684003990c7b3ab804792083fbc
Michael Kuts <mkuts12@gmail.com>
2016-02-14 23:05:51 +0200

    now path will be good. closes #1

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import Express from 'express';
+import path from 'path';
 
 let app = new Express();
 let port = process.env.NODE_ENV === 'production' ? 8085 : 8080;
 
 app.use('/', Express.static('./public'));
+
+app.get('/:album', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
 
 app.listen( port, () => {
     console.log( 'listening on port ' + port );
